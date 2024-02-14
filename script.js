9a34c0cc93f303dd08b9f5252beab47111bad2f7
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2024-02-14 22:57:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Navbar Section
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+// Home Section
+
+var typed = new Typed('#element', {
+  strings: ['Web Developer', 'Frontend Developer', 'PHP Developer', 'Python Developer', 'Web Designer'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+
+
+
+
+
+
+
+
+
+  
+ 
